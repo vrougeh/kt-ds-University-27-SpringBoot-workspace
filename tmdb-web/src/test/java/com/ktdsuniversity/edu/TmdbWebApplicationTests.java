94398d0dd2bf230842ba67fed6ce1326c7269168
vrougeh <dlaghdrbs907@gmail.com>
2026-03-26 16:51:57 +0900

    add : add tmdb project

---
+++ tmdb-web/src/test/java/com/ktdsuniversity/edu/TmdbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktdsuniversity.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
