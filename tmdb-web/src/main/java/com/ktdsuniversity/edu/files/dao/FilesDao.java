c6f8733c603b7fa4d8fc259a552645a2743c8a57
vrougeh <dlaghdrbs907@gmail.com>
2026-04-06 02:02:21 +0900

    add : add member code

--- tmdb-web/src/main/java/com/ktdsuniversity/edu/files/dao/FilesDao.java
+++ tmdb-web/src/main/java/com/ktdsuniversity/edu/files/dao/FilesDao.java
@@ -1,13 +1,10 @@
 package com.ktdsuniversity.edu.files.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ktdsuniversity.edu.files.vo.request.SearchFileVO;
 import com.ktdsuniversity.edu.files.vo.request.UploadVO;
 import com.ktdsuniversity.edu.files.vo.response.DownLoadVO;
-import com.ktdsuniversity.edu.movie.vo.request.UpdateVO;
 
 @Mapper
 public interface FilesDao {
@@ -15,13 +12,5 @@ public interface FilesDao {
 	int insertAttachFile(UploadVO uploadVO);
 
 	DownLoadVO selectFilesByGroupIdAndFileNum(SearchFileVO searchFileVO);
-
-	int deleteFilesByBoardId(String id);
-
-	List<String> selectFilePathByFileGroupIdAndFileNums(UpdateVO updateVO);
-
-	int deleteFilesByFileGroupIdAndFileNums(UpdateVO updateVO);
-
-	List<String> selectFilesByGroupId(String id);
 
 }
